ece8da1b2e4a002b01c032105060bc7aa1a1c86c
NMTHmations <125398293+NMTHmations@users.noreply.github.com>
2024-10-23 21:00:25 +0200

    Spring Boot Session schema added.

--- thebarn/src/main/java/com/sfm/thebarn/thebarn/ThebarnApplication.java
+++ thebarn/src/main/java/com/sfm/thebarn/thebarn/ThebarnApplication.java
@@ -1,5 +1,6 @@
 package com.sfm.thebarn.thebarn;
 
+import com.sfm.thebarn.thebarn.controller.SessionManager;
 import com.sfm.thebarn.thebarn.model.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,9 +10,9 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class ThebarnApplication {
 
 	public static void main(String[] args) {
-
 		ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(ThebarnApplication.class, args);
 		/*Just a simple test for Future Implementations*/
+		SessionManager sessionManager = configurableApplicationContext.getBean(SessionManager.class);
 		Farms farm = new Farms("9821398","Csonkas Tanya",3893,"Mogyoroska","Csonkas Tanya utca",1);
 		Users user = new Users("CSINEQ","KIRALY",farm);
 		FarmsCRUD repo = configurableApplicationContext.getBean(FarmsCRUD.class);
