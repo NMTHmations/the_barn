a9ae8ccf5ff79079f626abda411c9aa2a84fca53
davidcs8 <csutakdavid8@gmail.com>
2024-11-05 20:23:03 +0100

    Add sql query builder

--- thebarn/src/main/java/com/sfm/thebarn/thebarn/model/AnimalsCRUD.java
+++ thebarn/src/main/java/com/sfm/thebarn/thebarn/model/AnimalsCRUD.java
@@ -1,7 +1,10 @@
 package com.sfm.thebarn.thebarn.model;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AnimalsCRUD extends CrudRepository<Animals, String> {
