c4d4510dde0a3378e80fb3dd2cf48984ef51ade7
NMTHmations <125398293+NMTHmations@users.noreply.github.com>
2024-11-15 17:19:17 +0100

    Added Tests

--- thebarn/src/main/java/com/sfm/thebarn/thebarn/ThebarnApplication.java
+++ thebarn/src/main/java/com/sfm/thebarn/thebarn/ThebarnApplication.java
@@ -1,6 +1,7 @@
 package com.sfm.thebarn.thebarn;
 
 import com.sfm.thebarn.thebarn.model.*;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -12,8 +13,8 @@ public class ThebarnApplication {
 
 		ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(ThebarnApplication.class, args);
 		/*Just a simple test for Future Implementations*/
-		Farms farm = new Farms("9821398","Csonkas Tanya",3893,"Mogyoroska","Csonkas Tanya utca",1);
-		Users user = new Users("CSINEQ","KIRALY",farm);
+		Farms farm = new Farms("HU-12345-12345","Csonkas Tanya",3893,"Mogyoroska","Csonkas Tanya utca",1);
+		Users user = new Users("admin@example.com", DigestUtils.sha256Hex("SFM2024"),farm);
 		FarmsCRUD repo = configurableApplicationContext.getBean(FarmsCRUD.class);
 		UsersCRUD userrepo = configurableApplicationContext.getBean(UsersCRUD.class);
 		repo.save(farm);
