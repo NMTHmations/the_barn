1ebb80f10bfa4ee63fc9db75d667b7ef4b00d912
davidcs8 <csutakdavid8@gmail.com>
2024-11-28 17:25:26 +0100

    Add dropdown fields

--- thebarn/src/main/resources/templates/search-interface/src/components/Header.tsx
+++ thebarn/src/main/resources/templates/search-interface/src/components/Header.tsx
@@ -1,3 +1,7 @@
+import BreedDropdown from "./BreedDropdown";
+import ButtonDropdown from "./ButtonDropdown";
+import TypeDropdown from "./TypeDropdown";
+
 function Header() {
   return (
     <div className="container-fluid">
@@ -22,23 +26,13 @@ function Header() {
           <label className="checkbox-label"> bika</label>
         </div>
         <div className="col-sm-2 search-header-generic">
-          <select name="breed" className="dropdown-generic">
-            <option value="all" selected>
-              minden fajta
-            </option>
-          </select>
+          <BreedDropdown />
         </div>
         <div className="col-sm-2 search-header-generic">
-          <select name="type" className="dropdown-generic">
-            <option value="all" selected>
-              minden típus
-            </option>
-          </select>
+          <TypeDropdown />
         </div>
         <div className="col-sm-1 search-header-plus-btn">
-          <button className="btn-plus">
-            <i className="fa-solid fa-plus"></i>
-          </button>
+          <ButtonDropdown />
         </div>
       </div>
     </div>
