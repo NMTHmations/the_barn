0db941870effbafcc69d026e998d17972261c8f0
NMTHmations <125398293+NMTHmations@users.noreply.github.com>
2024-12-07 19:02:41 +0100

    Farm-controller error messages added

--- thebarn/src/main/java/com/sfm/thebarn/thebarn/controller/FarmController.java
+++ thebarn/src/main/java/com/sfm/thebarn/thebarn/controller/FarmController.java
@@ -12,11 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.regex.Pattern;
 
 @Controller
-@RequestMapping("/farm_registration")
+@RequestMapping("/farm-registration")
 public class FarmController {
 
     @Autowired
@@ -53,19 +54,28 @@ public class FarmController {
             @RequestParam("streetNumber") int streetNumber,
             @RequestParam("username") String username,
             @RequestParam("password") String password,
-            Model model
-    ) {
+            Model model,
+            RedirectAttributes redirectAttributes) {
         // Tenyészetkód validáció
         if (!FARM_ID_PATTERN.matcher(farmId).matches()) {
             model.addAttribute("errorMessage", "Helytelen tenyészetkód! Helyes formátum: HU-12345-12345");
             return "farm/registration";
         }
 
+        if (farmsCRUD.findById(farmId).isPresent()) {
+            model.addAttribute("errorMessage", "A megadott tenyészetkód már létezik!");
+            return "farm/registration";
+        }
+        if (usersCRUD.findById(username).isPresent()) {
+            model.addAttribute("errorMessage", "A megadott felhasználónév (e-mail cím) már létezik!");
+            return "farm/registration";
+        }
         Farms farm = new Farms(farmId, farmName, zipCode, settlement, street, streetNumber);
 
         farmsCRUD.save(farm);
 
         usersCRUD.save(new Users(username, DigestUtils.sha256Hex(password),farm));
+
 
         return "redirect:/";
     }
